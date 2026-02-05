95ea56f162a55a0dc30c78f93ed369712dc977c4
Shaina Nawaz <nawazshaina4@gmail.com>
2026-02-05 14:09:04 +0530

    Query and params

--- src/app.js
+++ src/app.js
@@ -2,33 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("hello test")
-});
-//get->it can only handle get call
+
+//req.query can give u info abt query param
 app.get("/user", (req, res) => {
-    res.send({name:"shaina"});
+    res.send(req.query);
 });
 
-
-app.post("/user", (req, res) => {
-    res.send("save data");
-});
-app.delete("/user", (req, res) => {
-    res.send("delete successfully")
+//dynamic route using /:
+app.get("/user/:userid/:name", (req, res) => {
+    res.send(req.params);
 });
 
-//use->it can handle all http method api call (get,post give same result) /hello/j
-app.use("/hello/j", (req, res) => {
-    res.send("helloascas")
-});
-app.use("/test", (req, res) => {
-    res.send("hello test")
-});
-
-app.use("/",(req, res) => {            //route handler( fisrt route ->this route overwrite every route and we give other route no work) code seq matter if we put / on the lst then it run on the lst
-    res.send("hello");
-});
 app.listen(3000, () => {
     console.log("running on port 3000")
 });
