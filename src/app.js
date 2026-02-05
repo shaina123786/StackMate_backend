5eb21c9346f3d080ee46903f6d42daf6b23c5d54
Shaina Nawaz <nawazshaina4@gmail.com>
2026-02-05 13:43:04 +0530

    Get,Post,Del

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {            //req handler
-    res.send("hello");
+app.use("/test", (req, res) => {
+    res.send("hello test")
+});
+//get->it can only handle get call
+app.get("/user", (req, res) => {
+    res.send({name:"shaina"});
 });
 
+
+app.post("/user", (req, res) => {
+    res.send("save data");
+});
+app.delete("/user", (req, res) => {
+    res.send("delete successfully")
+});
+
+//use->it can handle all http method api call (get,post give same result) /hello/j
+app.use("/hello/j", (req, res) => {
+    res.send("helloascas")
+});
+app.use("/test", (req, res) => {
+    res.send("hello test")
+});
+
+app.use("/",(req, res) => {            //route handler( fisrt route ->this route overwrite every route and we give other route no work) code seq matter if we put / on the lst then it run on the lst
+    res.send("hello");
+});
 app.listen(3000, () => {
     console.log("running on port 3000")
 });
